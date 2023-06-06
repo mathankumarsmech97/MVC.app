246cb1f8eb9b3a09a834d8792117046633e521ee
Mathankumar <109581628+mathankumarsmech97@users.noreply.github.com>
2023-06-06 09:42:39 +0530

    Add files via upload

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Repository;
+
+namespace MVCDemo.Controllers
+{
+    public class StudentController : Controller
+    {
+        
+        // GET: Student
+        public ActionResult Index()
+        {
+            ViewBag.Listname = "List from Viewbag";
+            ViewData["List"] = "List from view data";
+
+            ViewData.Add(new KeyValuePair<string, object>("Name", "Ramkumar"));
+            ViewData.Add(new KeyValuePair<string, object>("Dept", "Mech"));
+
+            //List<Student> list = new List<Student>();
+            //list.Add(new Student { StudentId = 1, Name = "Vijay", Department = "CSC"  });
+            //list.Add(new Student { StudentId = 2, Name = "Karthi", Department = "CSC"  });
+            //list.Add(new Student { StudentId = 3, Name = "Vickey", Department = "CSC" });
+
+            ViewData["Index"] = StudentRepository.GetAllStudents();
+            //ViewBag.Students = list;
+            //TempData["Telist"] = list;
+
+
+            return View();
+        }
+        public ActionResult Mylist()
+        {
+            ViewBag.List2 = TempData["Telist"];
+            TempData.Keep();
+            return RedirectToAction("Newlist");
+        }
+        public ActionResult Newlist()
+        {
+            ViewBag.List1 = TempData["Telist"];
+            return View();
+        }
+        public ActionResult Partiallist()
+        {
+            List<Student> list = new List<Student>();
+            list.Add(new Student { StudentId = 1, Name = "Vijay", Department = "CSC" });
+            list.Add(new Student { StudentId = 2, Name = "Karthi", Department = "CSC"});
+            list.Add(new Student { StudentId = 3, Name = "Vickey", Department = "CSC"});
+            ViewBag.Students = list;
+            return PartialView("Partial");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Department { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Dateofbirth { get; set; }
+
+        public string YearJoin { get; set; }
+
+        List<Student> Students { get; set; }
+    }
+}
